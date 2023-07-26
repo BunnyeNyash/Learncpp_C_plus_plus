9683653e22d2a9d6456686782599bf5a7b85aab8
BunnyeNyash <otachibosibori@gmail.com>
2023-07-26 17:12:09 +0300

    program that asks the user to enter an integer, waits for them to input an integer, then tells them what 2 times that number is

---
+++ multiplication_by2.cpp
@@ -0,0 +1,18 @@
+// This program asks the user to input a number of their choice and then it multiplies that number by 2
+
+#include <iostream> // for std::cin and std::cout
+
+int main()
+{
+	// Ask user for the number
+	std::cout << "Enter an integer: ";
+
+	// Create a variable to store the user's input
+	int num{};
+	std::cin >> num;
+
+	// Output the number multiplied by 2
+	std::cout << "Double " << num << " is: " << num * 2 << '\n';
+
+	return 0;
+}
